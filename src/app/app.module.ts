4836a5860909eeaa4b691a169f2fbe2ccd15f8f7
Ryan White <ryanwhite04@gmail.com>
2021-02-23 01:14:08 +0800

    Design basic layout using material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { GameComponent } from './game/game.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      FormsModule,
+      BrowserAnimationsModule,
+      MatSliderModule,
+      MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -6,10 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-
-  constructor() { }
+  minColumns: number = 4;
+  maxColumns: number = 10;
+  minRows: number = 4;
+  maxRows: number = 10;
+  minDifficulty: number = 0.1;
+  maxDifficulty: number = 0.9;
+  columns: number = 6;
+  rows: number = 8;
+  difficulty: number = 0.3;
+  cells: Cell[] = [];
+    constructor() {
+        this.reset();
+    }
 
   ngOnInit(): void {
   }
 
+  reset() {
+      console.log(this.columns, this.rows);
+      this.generateCells();
+  }
+
+  generateCells() {
+      let total = this.rows * this.columns;
+      this.cells = [...Array(total)].map((v, position) => new Cell({
+          position,
+          open: false,
+          mine: false,
+          flag: false,
+          count: 0,
+      }));
+  }
+
+}
+
+class Cell {
+
+    position: number;
+    open: boolean;
+    mine: boolean;
+    flag: boolean;
+    count: number;
+    constructor({ position, open, mine, flag, count }) {
+        this.position = position;
+        this.open = open;
+        this.mine = mine;
+        this.flag = flag;
+        this.count = count;
+    }
 }
